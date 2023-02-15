4072afb2ba2471216903f7f3687c3c22e38660ac
Daniel Toro <122295441+DanielT1234@users.noreply.github.com>
2023-02-15 01:25:05 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import math
 class Vector2d:
-    pass
+  def __init__(self,x,y):
+    self.x=x
+    self.y=y
+  def __repr__(self):
+    return f"Vector2d(x='{self.x}', y={self.y})"
+  def __str__(self):
+    return f"{self.x}:{self.y}"
+  def __abs__(self):
+    leg1=self.x**2
+    leg2=self.y**2
+    legs=leg1+leg2
+    return math.sqrt(legs)
+    
